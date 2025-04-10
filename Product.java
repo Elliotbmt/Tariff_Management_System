bba2c04ec049ff538f844de5354150676ef7b702
Elliot Boismartel <elliot.boismartel@gmail.com>
2025-04-10 06:02:19 -0400

    product class
    product class w/ constructor

---
+++ Product.java
@@ -0,0 +1,17 @@
+//---------------------------------------------------------------------------------------
+// Assignment 3
+// Question: class Product
+// Written by: Nithushan Kanapathippillai (40313552) and Elliot Boismartel (40315629)
+//---------------------------------------------------------------------------------------
+
+public class Product {
+    private String productName, country;
+    private double iPrice, fPrice;
+
+    public Product(String productName, String country, double iPrice, double fPrice){
+        this.productName=productName;
+        this.country=country;
+        this.iPrice=iPrice;
+        this.fPrice=fPrice;
+    }
+}
