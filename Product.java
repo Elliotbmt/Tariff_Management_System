3dbae510eca215c38ea240592b0907705ddd5c57
NithushanKana <nithushan.kana@gmail.com>
2025-04-10 23:05:25 -0400

    finish product class
    removed iprice and fprice, changed it to just price
    added getter/setter and toString
    added method to apply tarrifs
    might have to add a method to sort in alphabetical order

--- Product.java
+++ Product.java
@@ -5,13 +5,91 @@
 //---------------------------------------------------------------------------------------
 
 public class Product {
-    private String productName, country;
-    private double iPrice, fPrice;
+    private String productName, country, category;
+    private double price;
 
-    public Product(String productName, String country, double iPrice, double fPrice){
+    public Product(String productName, String country, String category, double price){
         this.productName=productName;
         this.country=country;
-        this.iPrice=iPrice;
-        this.fPrice=fPrice;
+        this.category=category;
+        this.price=price;
     }
+
+    //getters
+    public String getProductName() {
+        return productName;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public double getprice() {
+        return price;
+    }
+
+
+
+    //setters, we're reading from a file we dont need to set anything else
+    public void setprice(double price) {
+        this.price = price;
+    }
+
+    //method for tariff adjustment
+    public void applyTariff() {
+        switch (country) {
+            case "China":
+                price *= 1.25;
+                break;
+
+            case "USA":
+                if (category.equals("Electronics")) { price *= 1.10; }    
+                break;
+
+            case "Japan":
+                if (category.equals("Automobiles")) { price *= 1.15; }
+                break;
+
+            case "India":
+                if (category.equals("Agriculture")) { price *= 1.05; }
+                break;
+
+            case "South Korea":
+                if (category.equals("Electronics")) { price *= 1.08; }
+                break;
+
+            case "Saudi Arabia":
+                if (category.equals("Energy")) { price *= 1.12; }
+                break;
+
+            case "Germany":
+                if (category.equals("Manufacturing")) { price *= 1.06; }
+                break;
+
+            case "Bangladesh":
+                if (category.equals("Textile")) { price *= 1.04; }
+                break;
+
+            case "Brazil":
+                if (category.equals("Agriculture")) { price *= 1.09; }
+                break;
+        }
+    }
+
+    //method to sort in alphabetical order??
+    //...
+
+    //toString
+    @Override
+    public String toString() {
+        return productName + "," + country + "," + category + "," + price;
+    }
+
+
+
+
 }
---
+++ TariffApplier.java
@@ -0,0 +1,21 @@
+//---------------------------------------------------------------------------------------
+// Assignment 3
+// Question: 
+// Written by: Nithushan Kanapathippillai (40313552) and Elliot Boismartel (40315629)
+//---------------------------------------------------------------------------------------
+
+import java.io.*;
+import java.util.*;
+
+public class TariffApplier {
+
+
+
+
+
+
+
+
+
+    
+}
